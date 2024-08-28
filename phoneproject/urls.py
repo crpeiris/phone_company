a61093b5eea6f1dd0a871afd8525410dbf63a2c5
Ranil Peiris <cranil@sjp.ac.lk>
2024-08-28 20:46:49 +0200

    Store Home called from store views

--- phoneproject/urls.py
+++ phoneproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from store import views as storeviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    path('', storeviews.storehome),
 ]
