e966030c4c6ba22ed5b647c2179d8f94dd059be8
Ranil Peiris <cranil@sjp.ac.lk>
2024-08-28 20:11:26 +0200

    Chapter 1 complted

--- phoneproject/urls.py
+++ phoneproject/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from  . import settings
 from django.conf.urls.static import static 
-
+from store import views as storeviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    path ('', storeviews.storehome)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
