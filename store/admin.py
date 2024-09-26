59053de7786f88262b174cf4bd22f7e3decc72ad
Ranil Peiris <cranil@sjp.ac.lk>
2024-09-26 23:48:22 +0200

    All Models Added

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Customer, Product,Order,OrderProduct,Profile
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
+admin.site.register(Profile)
