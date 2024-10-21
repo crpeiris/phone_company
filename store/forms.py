142d2647b7caef361973aa9dcb7bf93c07ca5be5
Ranil Peiris <cranil@sjp.ac.lk>
2024-10-21 22:04:18 +0200

    Registration form updated

--- store/forms.py
+++ store/forms.py
@@ -12,3 +12,23 @@ class SignUpForm(UserCreationForm):
         model = User
          # Fields from User model to be included in the form
         fields = ['username','email', 'first_name', 'last_name', 'password1', 'password2']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class']='form-control'
+        self.fields['username'].widget.attrs['placeholder ']='User Name'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class ="form-text text-muted"><small>Required. 150 characters</span>'
+
+        self.fields['email'].widget.attrs['class']='form-control'
+        self.fields['email'].widget.attrs['placeholder ']='Email Address'
+        self.fields['email'].label = ''
+
+        self.fields['first_name'].widget.attrs['class']='form-control'
+        self.fields['first_name'].widget.attrs['placeholder ']='First Name'
+        self.fields['first_name'].label = ''
+
+        self.fields['last_name'].widget.attrs['class']='form-control'
+        self.fields['last_name'].widget.attrs['placeholder ']='Last Name'
+        self.fields['last_name'].label = ''
