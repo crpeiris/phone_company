6a71b01db068eb1ab54033df678b9143df98fb06
Ranil Peiris <cranil@sjp.ac.lk>
2024-10-30 16:44:16 +0100

    add cart items models

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product,Category
+from .models import CartItem, Product,Category
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import SignUpForm
@@ -71,3 +71,24 @@ def shop(request, category = None):
 def product(request, product_id):
     product = Product.objects.get(id=product_id)
     return render(request, 'store/product.html', {'product' : product, 'title': product.name })
+
+#Views for Cartitems management
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(product=product, 
+                                                       user=request.user)
+    cart_item.quantity += 1
+    cart_item.save()
+    return redirect('cart:view_cart')
+
+def view_cart(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(request, 'myapp/cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+def remove_from_cart(request, item_id):
+    cart_item = CartItem.objects.get(id=item_id)
+    cart_item.delete()
+    return redirect('cart:view_cart')
+
